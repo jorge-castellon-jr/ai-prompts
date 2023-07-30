f91dc17ea6199c10ce4e64e061df83d5d921be41
Jorge Castellon Jr <jorge@castellon.dev>
2023-07-30 00:26:17 -0700

    Restarted to get Tailwind working again

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.scss";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "AI Prompts",
+  description: "Prompts for AI-generated art",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
